7121a100add033b516847473f7b3c92f05ba5d6c
Martin Holen <martinholen@gmail.com>
2018-10-09 15:05:47 +0200

    Added movement to the drone, still lacking rotation though...

---
+++ Helicopter/Assets/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject chopper;
+
+    private Vector3 offset;
+
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - chopper.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position = chopper.transform.position + offset;
+	}
+}
---
+++ Helicopter/Assets/DroneMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    public int force = 100;
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (Input.GetKey("space"))
+        {
+            rb.AddForce(0, force * Time.deltaTime, 0);
+        }
+
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(-force * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(force * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(0, 0, -force * Time.deltaTime);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(0, 0, force * Time.deltaTime);
+        }
+
+    }
+}
