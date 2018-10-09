7ebf1b42b85466ed382920465a7a1bfdd0a2b091
Martin Holen <martinholen@gmail.com>
2018-10-09 22:51:35 +0200

    ARE YOU NOT ENTERTAINED?

--- Helicopter/Assets/Scripts/DroneMovement.cs
+++ Helicopter/Assets/Scripts/DroneMovement.cs
@@ -11,10 +11,26 @@ public class DroneMovement : MonoBehaviour {
     public Vector3 Left;
     public Vector3 Right;
 
-    public int force = 1000;
+    public Vector3 FrontForce;
+    public Vector3 BackForce;
+    public Vector3 LeftForce;
+    public Vector3 RightForce;
+
+    public int upForce = 700;
+    public int force = 10;
+    public int distance = 2;
 
     void Start()
     {
+        Front.Set(0, 0, -distance);
+        Back.Set(0, 0, -distance);
+        Left.Set(distance, 0, 0);
+        Right.Set(distance, 0, 0);
+
+        FrontForce.Set(0, 0, -force * Time.deltaTime);
+        BackForce.Set(0, 0, force * Time.deltaTime);
+        LeftForce.Set(-force * Time.deltaTime, 0, 0);
+        RightForce.Set(force * Time.deltaTime, 0, 0);
         Debug.Log("I fucked jakobs mom ('''\\_(o.O)_/''')"); //forceFr = force * Time.deltaTime, 0, 0;
     }
 
@@ -22,27 +38,28 @@ public class DroneMovement : MonoBehaviour {
     void FixedUpdate () {
         if (Input.GetKey("space"))
         {
-            rb.AddForce(0, force * Time.deltaTime, 0);
+            rb.AddForce(0, upForce * Time.deltaTime, 0);
         }
 
         if (Input.GetKey("w"))
         {
-            rb.AddForce(-force * Time.deltaTime, 0, 0);
+            rb.AddForceAtPosition(Front, FrontForce);
+            //rb.AddForce(-force * Time.deltaTime, 0, 0);
         }
 
         if (Input.GetKey("s"))
         {
-            rb.AddForce(force * Time.deltaTime, 0, 0);
+            rb.AddForceAtPosition(Back, BackForce);
         }
 
         if (Input.GetKey("a"))
         {
-            rb.AddForce(0, 0, -force * Time.deltaTime);
+            rb.AddForceAtPosition(Left, LeftForce);
         }
 
         if (Input.GetKey("d"))
         {
-            rb.AddForce(0, 0, force * Time.deltaTime);
+            rb.AddForceAtPosition(Right, RightForce);
         }
 
     }
