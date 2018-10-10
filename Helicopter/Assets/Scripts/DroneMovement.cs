5bd88a5db128d7dd53611eb55c791590682589d0
Martin Holen <martinholen@gmail.com>
2018-10-10 14:01:52 +0200

    Reworked the drone(quadcopter) since there were some bugs with it

--- Helicopter/Assets/Scripts/DroneMovement.cs
+++ Helicopter/Assets/Scripts/DroneMovement.cs
@@ -11,31 +11,41 @@ public class DroneMovement : MonoBehaviour {
     public Vector3 Left;
     public Vector3 Right;
 
+    public GameObject chopper;
+    public GameObject frontTrans;
+    public GameObject backTrans;
+
     public Vector3 FrontForce;
     public Vector3 BackForce;
     public Vector3 LeftForce;
     public Vector3 RightForce;
 
+    public Vector3 dirForce;
+
     public int upForce = 700;
-    public int force = 100;
+    public int force = 10;
     public int distance = 2;
 
     void Start()
     {
-        Front.Set(0, 0, -distance);
-        Back.Set(0, 0, -distance);
+
+        //transform.position - chopper.transform.position;
+        Front = frontTrans.transform.position - transform.position;
+        Back = backTrans.transform.position - transform.position;
+        //Back.Set(0, 0, -distance);
         Left.Set(distance, 0, 0);
         Right.Set(distance, 0, 0);
 
-        FrontForce.Set(0, 0, -force * Time.deltaTime);
-        BackForce.Set(0, 0, force * Time.deltaTime);
-        LeftForce.Set(-force * Time.deltaTime, 0, 0);
-        RightForce.Set(force * Time.deltaTime, 0, 0);
+        FrontForce.Set(0, force * Time.deltaTime, 0);
+        BackForce.Set(0, force * Time.deltaTime, 0);
+        LeftForce.Set(0, force * Time.deltaTime, 0);
+        RightForce.Set(0, force * Time.deltaTime, 0);
         Debug.Log("I fucked jakobs mom ('''\\_(o.O)_/''')"); //forceFr = force * Time.deltaTime, 0, 0;
     }
 
 
     void FixedUpdate () {
+        dirForce.Set(0, force * Time.deltaTime, 0);
         if (Input.GetKey("space"))
         {
             rb.AddForce(0, upForce * Time.deltaTime, 0);
@@ -43,14 +53,14 @@ public class DroneMovement : MonoBehaviour {
 
         if (Input.GetKey("w"))
         {
-            //rb.AddForceAtPosition(Front, FrontForce);
-            rb.AddForce(force * Time.deltaTime, 0, 0);
+            rb.AddForceAtPosition(Front.normalized, dirForce);
+            //rb.AddForce(force * Time.deltaTime, 0, 0);
         }
 
         if (Input.GetKey("s"))
         {
-            rb.AddForce(-force * Time.deltaTime, 0, 0);
-            //rb.AddForceAtPosition(Back, BackForce);
+            //rb.AddForce(-force * Time.deltaTime, 0, 0);
+            rb.AddForceAtPosition(Back.normalized, dirForce);
         }
 
         if (Input.GetKey("a"))
